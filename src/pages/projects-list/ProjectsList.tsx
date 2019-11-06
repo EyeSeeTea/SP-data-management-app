e524fe2c547f4002f7f02b77752c1ca8bffc710b
Adrian Quintana <adrianquintana@gmail.com>
2019-11-06 22:27:38 +0000

    delete unused import

--- src/pages/projects-list/ProjectsList.tsx
+++ src/pages/projects-list/ProjectsList.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { OldObjectsTable } from "d2-ui-components";
-import { ObjectsTable } from "d2-ui-components";
-
 
 import i18n from "../../locales";
 import PageHeader from "../../components/page-header/PageHeader";
