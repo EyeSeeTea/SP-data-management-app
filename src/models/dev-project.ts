b338a338b91deba35d453ace1274dd13f2a243fc
Arnau Sanchez <pyarnau@gmail.com>
2020-01-30 11:16:43 +0100

    Fix new livelihood sector

--- src/models/dev-project.ts
+++ src/models/dev-project.ts
@@ -23,7 +23,7 @@ export function getDevProject(initialProject: Project, enabled: boolean) {
         })
         .set("sectors", [
             { id: "mGQ5ckOTU8A", displayName: "Agriculture", code: "SECTOR_AGRICULTURE" },
-            { id: "m4Cg6FOPPR7", displayName: "Livelihoods", code: "SECTOR_LIVELIHOODS" },
+            { id: "GkiSljtLcOI", displayName: "Livelihood", code: "SECTOR_LIVELIHOOD" },
         ])
         .set(
             "dataElements",
