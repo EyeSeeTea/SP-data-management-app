18f853937dbd9a5c909bb12eee2590e2f7bb2479
Adrian Quintana <adrianquintana@gmail.com>
2020-01-30 12:33:12 +0000

    lint

--- src/pages/data-approval/DataApproval.tsx
+++ src/pages/data-approval/DataApproval.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState, useEffect } from "react";
 import moment from "moment";
 import { useHistory, useRouteMatch } from "react-router";
