9df8f3667a6f681f99d9b0ebf0cb9bbc3bf224eb
Arnau Sanchez <pyarnau@gmail.com>
2020-07-01 12:39:01 +0200

    [Sharing] Show only users in country in search box

--- src/components/steps/sharing/SharingStep.tsx
+++ src/components/steps/sharing/SharingStep.tsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+import i18n from "../../../locales";
 import { D2Api } from "../../../types/d2-api";
 import { Sharing, ShareUpdate } from "d2-ui-components";
 import { StepProps } from "../../../pages/project-wizard/ProjectWizard";
 import ProjectSharing from "../../../models/ProjectSharing";
+import Project from "../../../models/Project";
 
 const showOptions = {
     title: false,
@@ -12,12 +16,20 @@ const showOptions = {
     permissionPicker: false,
 };
 
-function searchUsers(api: D2Api, query: string) {
-    const options = {
+function searchUsers(api: D2Api, query: string, project: Project) {
+    const countryId = project.parentOrgUnit ? project.parentOrgUnit.id : undefined;
+    const userOptions = {
+        fields: { id: true, displayName: true },
+        filter: {
+            displayName: { ilike: query },
+            ...(countryId ? { "organisationUnits.id": { eq: countryId } } : {}),
+        },
+    };
+    const userGroupOptions = {
         fields: { id: true, displayName: true },
         filter: { displayName: { ilike: query } },
     };
-    return api.metadata.get({ users: options, userGroups: options }).getData();
+    return api.metadata.get({ users: userOptions, userGroups: userGroupOptions }).getData();
 }
 
 const SharingStep: React.FC<StepProps> = props => {
@@ -32,21 +44,36 @@ const SharingStep: React.FC<StepProps> = props => {
         },
         [projectSharing, onChange]
     );
-    const search = React.useCallback((query: string) => searchUsers(api, query), [api]);
+    const search = React.useCallback((query: string) => searchUsers(api, query, project), [api]);
+
+    const classes = useStyles();
 
     const unremovebleIds = React.useMemo(() => {
         return projectSharing.getBaseSharingIds();
     }, [projectSharing]);
 
     return (
-        <Sharing
-            meta={sharedObject}
-            showOptions={showOptions}
-            onSearch={search}
-            onChange={setProjectSharing}
-            unremovebleIds={unremovebleIds}
-        />
+        <React.Fragment>
+            <Sharing
+                meta={sharedObject}
+                showOptions={showOptions}
+                onSearch={search}
+                onChange={setProjectSharing}
+                unremovebleIds={unremovebleIds}
+            />
+
+            <div className={classes.footer}>
+                {i18n.t(
+                    "Note: only users with permissions in the selected country ({{country}}) can be added to this project",
+                    { country: project.parentOrgUnit?.displayName }
+                )}
+            </div>
+        </React.Fragment>
     );
 };
+
+const useStyles = makeStyles({
+    footer: { marginTop: 10, fontSize: "1.2em" },
+});
 
 export default React.memo(SharingStep);
