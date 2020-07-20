8eb15aa607f09435517cdb604e7938822cf0b40b
Arnau Sanchez <pyarnau@gmail.com>
2020-07-20 12:04:34 +0200

    Update tests to latest metadata

--- src/models/__tests__/project-data.ts
+++ src/models/__tests__/project-data.ts
@@ -48,8 +48,8 @@ export function getProject<K extends keyof ProjectData>(
 ): Project {
     return Project.create(api, config)
         .setObj(Object.assign({}, projectData, partialProjectData || {}))
-        .updateDataElementsSelection("mGQ5ckOTU8A", ["WS8XV4WWPE7", "ik0ICagvIjm"])
+        .updateDataElementsSelection("ieyBABjYyHO", ["WS8XV4WWPE7", "ik0ICagvIjm"])
         .project.updateDataElementsSelection("GkiSljtLcOI", ["yMqK9DKbA3X"])
-        .project.updateDataElementsMERSelection("mGQ5ckOTU8A", ["WS8XV4WWPE7"])
+        .project.updateDataElementsMERSelection("ieyBABjYyHO", ["WS8XV4WWPE7"])
         .project.updateDataElementsMERSelection("GkiSljtLcOI", ["yMqK9DKbA3X"]).project;
 }
---
+++ src/utils/tests.ts
@@ -0,0 +1,25 @@
+import fs from "fs";
+import MockAdapter from "axios-mock-adapter/types";
+
+export function logUnknownRequest(mock: MockAdapter) {
+    mock.onAny().reply(args => {
+        const { method, url, params, data } = args;
+        const msgs: string[] = [];
+        msgs.push(`Error:  ${method?.toUpperCase()} ${url}`);
+        if (params) msgs.push(`Params: ${JSON.stringify(params)}`);
+        if (data) {
+            const json = JSON.stringify(JSON.parse(data), null, 2);
+            if (json.length > 100) {
+                const n = Math.floor(Math.random() * 1000) + 1;
+                const actualPath = `/tmp/actual-${n}.json`;
+                fs.writeFileSync(actualPath, json);
+                msgs.push(`Data written: ${actualPath}`);
+            } else {
+                msgs.push(`Data: ${JSON.stringify(JSON.parse(data), null, 2)}`);
+            }
+        }
+
+        console.debug(msgs.join("\n"));
+        return [500, {}];
+    });
+}
