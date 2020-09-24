727fd9bdb47202495e5e808450ec986ed74d8487
Arnau Sanchez <pyarnau@gmail.com>
2020-09-24 12:08:51 +0200

    Use only new for download cumulatives

--- src/models/ProjectDownload.ts
+++ src/models/ProjectDownload.ts
@@ -195,6 +195,11 @@ class ProjectDownload {
             { fill: fills.cumulative }
         );
 
+        const cumulativeNewSumFormula = formula(
+            (rowIdx, colIdx) => [cell(colIdx, rowIdx + 1), "+", cell(colIdx, rowIdx + 2)].join(""),
+            { fill: fills.cumulative }
+        );
+
         // target header+data (1 + 4) + achievement (1) + actual header+data (1 + 4)
         const rowsCountByDataElement = 11;
 
@@ -217,13 +222,10 @@ class ProjectDownload {
                     header(i18n.t("Total People Targeted"), { fill: fills.target }),
                     ...this.mapPeriods(
                         period =>
-                            analytics.get(dataElement, period.id, [
-                                config.categoryOptions.target,
-                                config.categoryOptions.new,
-                            ]),
+                            analytics.get(dataElement, period.id, [config.categoryOptions.target]),
                         { font: { bold: true }, fill: fills.target }
                     ),
-                    { ...sumRowFormula },
+                    cumulativeNewSumFormula,
                     text("", { merge: [1, 5] }), // Method
                     text("", { merge: [1, 5] }), // Responsible
                 ],
@@ -308,13 +310,10 @@ class ProjectDownload {
                     header(i18n.t("Total Actual People"), { fill: fills.actual }),
                     ...this.mapPeriods(
                         period =>
-                            analytics.get(dataElement, period.id, [
-                                config.categoryOptions.actual,
-                                config.categoryOptions.new,
-                            ]),
+                            analytics.get(dataElement, period.id, [config.categoryOptions.actual]),
                         { fill: fills.actual }
                     ),
-                    sumRowFormula,
+                    cumulativeNewSumFormula,
                 ],
                 [
                     ...repeat(empty, 4),
