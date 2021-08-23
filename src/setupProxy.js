af1aea8c371aee9c16bc4fb01b54e80d2a78bb23
Arnau Sanchez <pyarnau@gmail.com>
2021-08-23 15:32:15 +0200

    Redirect visualization DHIS2 apps to original URL

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,15 +1,31 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
+const redirectPaths = ["/dhis-web-pivot", "/dhis-web-data-visualizer"];
+const envVarName = "REACT_APP_DHIS2_BASE_URL";
+
 module.exports = function (app) {
-    const targetUrl = process.env.REACT_APP_DHIS2_BASE_URL;
-    if (!targetUrl) return;
+    const targetUrl = process.env[envVarName];
+    if (!targetUrl) {
+        console.error(`Set ${envVarName} to base DHIS2 URL`);
+        process.exit(1);
+    }
 
     const proxy = createProxyMiddleware({
         target: targetUrl,
         logLevel: "debug",
         changeOrigin: true,
         pathRewrite: { "^/dhis2/": "/" },
+        onProxyReq: function (proxyReq, req, res) {
+            const { path } = proxyReq;
+            const shouldRedirect = redirectPaths.some(p => path.startsWith(p));
+
+            if (shouldRedirect) {
+                const url2 = targetUrl.replace(/\/$/, "") + path;
+                res.location(url2);
+                res.sendStatus(302);
+            }
+        },
     });
 
     app.use(["/dhis2"], proxy);
