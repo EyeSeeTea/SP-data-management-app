533b711146cd46feb0e88bc6a781cc6d3a1b3ca2
deeonwuli <onwulidumebi@gmail.com>
2022-09-13 10:54:33 +0100

    use generateUrl function to get dataApprovalLink

--- src/models/ProjectNotification.ts
+++ src/models/ProjectNotification.ts
@@ -58,16 +58,11 @@ export class ProjectNotification {
         const { displayName: user, username } = this.currentUser.data;
         const subject = i18n.t("{{username}} is requesting a data review", { username });
 
-        const pageOrigin = window.location.origin;
         const projectId = this.project.id;
-        const dataApprovalLink =
-            pageOrigin +
-            "/api/apps/Data-Management-App/index.html#/data-approval/" +
-            projectId +
-            "/" +
-            dataSetType +
-            "/" +
-            period;
+        const path = generateUrl("dataApproval", { id: projectId, dataSetType, period });
+        const dataApprovalLink = getFullUrl(path);
+
+        console.log(dataApprovalLink);
 
         const users = res.userRoles.flatMap(userRole => userRole.users);
         const dataSet = res.dataSets[0];
@@ -173,7 +168,7 @@ The reason provided by the user was:
             }),
 
             // Cypress fails when body includes an URL,
-            !this.isTest ? getUrl(project) : "test-url",
+            !this.isTest ? getProjectUrl(project) : "test-url",
             html2Text(element),
         ];
 
@@ -224,7 +219,11 @@ function html2Text(element: ReactElement): string {
     return textWithoutBlankLines;
 }
 
-function getUrl(project: Project) {
+function getProjectUrl(project: Project) {
     const path = generateUrl("projects", undefined, { search: project.code });
+    return getFullUrl(path);
+}
+
+function getFullUrl(path: string): string {
     return window.location.href.split("#")[0] + "#" + path;
 }
