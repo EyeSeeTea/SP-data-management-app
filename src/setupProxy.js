9af7681c2dbc978082424aaf5d128f7aab4a49bb
Arnau Sanchez <pyarnau@gmail.com>
2023-02-07 11:54:44 +0100

    Use latest version of setupProxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,28 +1,44 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
+/* react-script automatically executes src/setupProxy.js on init. Tasks:
+
+    - Proxy requests from /dhis2/xyz to $REACT_APP_DHIS2_BASE_URL/xyz. Reason: Avoid problems with
+      CORS and cross-domain cookies, as the app connects only to the local development server.
+
+    - Redirect paths in `redirectPaths` to the original DHIS2 URL. Reason: some apps, i.e. Pivot Table App,
+      do not work through the proxy. Tipically, these links are rendered on iframed dashboards.
+*/
+
 const redirectPaths = ["/dhis-web-pivot", "/dhis-web-data-visualizer"];
-const envVarName = "REACT_APP_DHIS2_BASE_URL";
+
+const dhis2UrlVar = "REACT_APP_DHIS2_BASE_URL";
+const dhis2AuthVar = "REACT_APP_DHIS2_AUTH";
+const proxyLogLevel = "REACT_APP_PROXY_LOG_LEVEL";
 
 module.exports = function (app) {
-    const targetUrl = process.env[envVarName];
+    const targetUrl = process.env[dhis2UrlVar];
+    const auth = process.env[dhis2AuthVar];
+    const logLevel = process.env[proxyLogLevel] || "error";
+
     if (!targetUrl) {
-        console.error(`Set ${envVarName} to base DHIS2 URL`);
+        console.error(`Set ${dhis2UrlVar} to base DHIS2 URL`);
         process.exit(1);
     }
 
     const proxy = createProxyMiddleware({
         target: targetUrl,
-        logLevel: "error",
+        auth,
+        logLevel,
         changeOrigin: true,
-        pathRewrite: { "^/dhis2": "/" },
-        onProxyReq: function (proxyReq, req, res) {
+        pathRewrite: { "^/dhis2/": "/" },
+        onProxyReq: function (proxyReq, _req, res) {
             const { path } = proxyReq;
-            const shouldRedirect = redirectPaths.some(p => path.startsWith(p));
+            const shouldRedirect = redirectPaths.some(redirectPath => path.startsWith(redirectPath));
 
             if (shouldRedirect) {
-                const url2 = targetUrl.replace(/\/$/, "") + path;
-                res.location(url2);
+                const redirectUrl = targetUrl.replace(/\/$/, "") + path;
+                res.location(redirectUrl);
                 res.sendStatus(302);
             }
         },
